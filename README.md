97cfc15653e2f82231e273a3037240fdfef769bb
Joshua Perrett <jperrett256@gmail.com>
2023-08-07 21:19:38 +0100

    made build script a single file

--- README.md
+++ README.md
@@ -16,6 +16,9 @@ Building dynamorio requires `apt install libipt-dev zlib1g-dev liblz4-dev` at a 
 
 See the sections below about version issues with dependencies installed with apt, and how to resolve them.
 
+A build script has been prepared to make the process easier. It should boot into cheribsd during the build process, use `ctrl+A` followed by `x` to exit.
+While this build script should just work, provided all dependencies are provided (and are the appropriate versions - see below), I recommend **starting this process long before you need to use these tools**. Both cheribuild and g++ have flags for producing verbose output, which may be helpful if they complain.
+
 ### Version Issues - Ubuntu 18.04
 
 The version of `liblz4-dev` found on Ubuntu 18.04 is too old for dynamorio and cheri-trace-converter. A more up-to-date release can be downloaded from the [lz4 GitHub page](https://github.com/lz4/lz4/releases) or the [Ubuntu packages archive](https://packages.ubuntu.com/focal/liblz4-dev). A local installation may be performed with:
---
+++ build.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -e
+
+if [ "$#" -ne 1 ] || ! [ -f "$1" ]; then
+  echo "Usage: $0 <SPEC .iso path>" >&2
+  exit 1
+fi
+spec_path=$1
+
+# snippet taken from: https://stackoverflow.com/a/246128/6887828
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+########################### build qemu and cheribsd ###########################
+
+$script_dir/cheribuild/cheribuild.py --source-root $script_dir/cheri qemu --qemu/configure-options="--enable-drcachesim-log-instr"
+$script_dir/cheribuild/cheribuild.py --source-root $script_dir/cheri -d run-riscv64-purecap --run-riscv64-purecap/ephemeral \
+    --run-riscv64-purecap/extra-options="--icount shift=0,align=off --cheri-trace-backend drcachesim --cheri-trace-drcachesim-tracefile /dev/null --cheri-trace-drcachesim-dbgfile /dev/null"
+
+############# build SPEC binaries and populate qemu image with it #############
+
+$script_dir/cheribuild/cheribuild.py --source-root $script_dir/cheri spec2006-riscv64-purecap --spec2006-riscv64-purecap/iso-path $spec_path
+$script_dir/cheribuild/cheribuild.py --source-root $script_dir/cheri disk-image-riscv64-purecap
+
+#################### build dynamorio (includes drcachesim) ####################
+
+mkdir -p $script_dir/dynamorio_build
+pushd $script_dir/dynamorio_build
+cmake ../dynamorio
+make -j
+popd
+
+############################### build traceconv ###############################
+
+pushd $script_dir/cheri-trace-converter
+make
+popd
